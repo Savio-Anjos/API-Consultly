d9b150a47de852ebe98aa765edfe621cd833ca94
Savio-Anjos <savioanjos2020@gmail.com>
2024-01-01 20:57:58 -0300

    feat :sparkles: Added create user use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,8 +1,20 @@
 import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
+import { randomUUID } from "crypto";
 
 export class InMemoryUsersRepository implements UsersRepository {
-  create(data: Prisma.UserCreateInput): Promise<User> {
-    throw new Error("Method not implemented.");
+  public itens: User[] = [];
+
+  public async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+    };
+
+    this.itens.push(user);
+
+    return user;
   }
 }
