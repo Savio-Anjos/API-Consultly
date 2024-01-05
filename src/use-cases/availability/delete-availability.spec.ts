44cc89cc94f8b1ee73de393a8cbbee2efaef6a69
Savio-Anjos <savioanjos2020@gmail.com>
2024-01-05 16:20:30 -0300

    test :white_check_mark: Added unitary tests for delete availability use case

---
+++ src/use-cases/availability/delete-availability.spec.ts
@@ -0,0 +1,44 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { hash } from "bcryptjs";
+import { AvailabilityRepository } from "@/repositories/availability-repository";
+import { InMemoryAvailabilityRepository } from "@/repositories/in-memory/in-memory-availability-repository";
+import { DeleteAvailabilityUseCase } from "./delete-availability";
+import { ConsultantsRepository } from "@/repositories/consultants-repository";
+import { InMemoryConsultantsRepository } from "@/repositories/in-memory/in-memory-consultants-repository";
+
+let availabilityRepository: AvailabilityRepository;
+let consultantRepository: ConsultantsRepository;
+let sut: DeleteAvailabilityUseCase;
+describe("Delete availability Use Case", () => {
+  beforeEach(() => {
+    availabilityRepository = new InMemoryAvailabilityRepository();
+    consultantRepository = new InMemoryConsultantsRepository();
+    sut = new DeleteAvailabilityUseCase(availabilityRepository);
+  });
+
+  it("should be able to deçlete availability", async () => {
+    const consultant = await consultantRepository.create({
+      name: "John Doe",
+      email: "jonhdoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    await availabilityRepository.create({
+      day: "25",
+      startTime: "18:00",
+      endTime: "19:00",
+      consultantId: consultant.id,
+    });
+
+    const { id } = await availabilityRepository.create({
+      day: "25",
+      startTime: "18:00",
+      endTime: "19:00",
+      consultantId: consultant.id,
+    });
+
+    const { availabilities } = await sut.execute({ id });
+
+    expect(availabilities).toHaveLength(1);
+  });
+});
